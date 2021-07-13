e0b016efb1e75ca6f1b724878ca5ac5dab0f5619
EC2 Default User <ec2-user@ip-172-31-31-64.us-west-2.compute.internal>
2021-07-13 07:55:57 +0000

    update groovy script

--- nodejs.groovy
+++ nodejs.groovy
@@ -1,18 +1,17 @@
-job('NodeJS example') { // Job NAME
-    scm { // Configure Source control management 
-        git('git://github.com/yanivomc/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('DSL User')
+job('NodeJS example') {
+    scm {
+        git('git://github.com/yanivomc/docker-demo.git') {  node ->
+            node / gitConfigName('DSL User)
             node / gitConfigEmail('jenkins-dsl@domain.com)
         }
     }
-    triggers { // Configure when to check for changes 
+    triggers { 
         scm('H/5 * * * *')
     }
     wrappers {
-        nodejs('nodejs') // this is the name of the NodeJS installation in 
-                         // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
+        nodejs('nodejs')
     }
-    steps { // what steps to take 
+    steps { 
         shell("npm install")
     }
 }
